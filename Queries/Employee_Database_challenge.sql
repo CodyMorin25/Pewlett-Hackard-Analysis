b4bae2ed9073a604b54454e06d87d17ba035389d
Cody Morin <cpmorin94@gmail.com>
2022-02-13 01:49:15 -0500

    added csv files, challenge file

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,50 @@
+-- Deliverable 1
+SELECT e.emp_no, 
+       e.first_name, 
+       e.last_name,
+       t.title, 
+       t.from_date, 
+       t.to_date
+FROM employees as e
+FROM titles as t
+INTO retirement_titles
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+WHERE (to_date = '9999-01-01')
+ORDER BY emp_no , to_date DESC;
+
+
+SELECT COUNT(u.emp_no), u.title
+INTO retiring_titles
+FROM unique_titles as u
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+
+-- Deliverable 2
+
+SELECT DISTINCT ON(e.emp_no)e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibilty
+FROM employees AS e
+	INNER JOIN dept_emp AS de
+		ON (e.emp_no = de.emp_no)
+			INNER JOIN titles AS t
+				ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
